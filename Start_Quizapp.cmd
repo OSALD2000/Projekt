0068cbe218abfa8c52289fe68d92dfef89a3e452
osama.aldalati <osama.aldalati@ivfp.de>
2023-11-08 11:28:48 +0100

    start

--- Start_Quizapp.cmd
+++ Start_Quizapp.cmd
@@ -3,39 +3,6 @@
 for %%I in ("%~dp0.") do set "script_dir=%%~fI"
 
 cd %script_dir%
-cd .. 
 
-start cmd /k "code Projekt"
-
-start cmd /k "cd Server && npm i"
-
-start cmd /k "cd UI && npm i"
-
-timeout /t 80
-
-start cmd /k "cd Server && npm start"
-
-start cmd /k "cd UI && npm start"
-
-:input
-echo "Entwicklung Ubgebungen Gestarten : ) geben Sie exit zum beenden"
-set /p userInput=
-
-:while_loop
-if "%userInput%" NEQ "beenden" (
-	:input
-	echo "Entwicklung Ubgebungen Gestarten : ) geben Sie exit zum beenden"
-	set /p userInput=
-	
-)
-
-set "ProcessName=Code.exe"
-
-tasklist /FI "IMAGENAME eq %ProcessName%" | find /I "%ProcessName%" > nul
-
-if %errorlevel%==0 (
-	taskkill /F /IM Code.exe
-    echo The process %ProcessName% is killed.
-) else (
-    echo The process %ProcessName% is not running.
-)
+start cmd /k "code Server && exit"
+start cmd /k "code UI && exit"
