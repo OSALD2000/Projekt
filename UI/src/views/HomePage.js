0da4bd1b08ec8180fc4c7d8386c555c9bb5b9fce
osama.aldalati <osama.aldalati@ivfp.de>
2023-11-10 20:41:10 +0100

    logout

--- UI/src/views/HomePage.js
+++ UI/src/views/HomePage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Home = (props) => {
-  return <h1 style={{textAlign: "center", marginTop:"10%"}}>Erfolgreich Angemeldet</h1>;
+  const loguthandler = () => {
+    localStorage.clear();
+  };
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Erfolgreich Angemeldet
+      </h1>
+      <div className="logout">
+        <NavLink onClick={loguthandler}>LogOut</NavLink>
+      </div>
+    </>
+  );
 };
 
 export default Home;
