f7bba15c0ce08595c03ca3b58124e9925766a61f
osama.aldalati <osama.aldalati@ivfp.de>
2024-01-14 12:56:10 +0100

    env anpassung

--- Server/util/db.js
+++ Server/util/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("test", "root", "Osama", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize(process.env.SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
 });
 
 module.exports = sequelize;
